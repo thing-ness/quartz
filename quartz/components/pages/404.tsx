7a3035b43772a44f6d2cd1ec059089fb8dd5a650
obsidian-joh <obsidian@joh.net>
2026-06-25 10:28:21 +0200

    Update 404.tsx

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -7,7 +7,7 @@ const NotFound: QuartzComponent = ({ cfg, ctx }: QuartzComponentProps) => {
 
   return (
     <article class="popover-hint">
-      <h1>404</h1>
+      <h1>oops! you're not here...</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
       <script
